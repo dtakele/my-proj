51305b31a2d276454404d5ad866013c66b5e1ddc
Dejene Ayana <dtakele@gmail.com>
2016-10-20 00:53:04 -0500

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {Hero} from './hero';
+
+@Component({
+  selector: 'my-app',
+  templateUrl:'mytemplate.html'
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+heroes = [
+  new Hero(1, 'Windstorm'),
+  new Hero(13, 'Bombasto'),
+  new Hero(15, 'Magneta'),
+  new Hero(20, 'Tornado')
+];
+myHero = this.heroes[0];
+}
